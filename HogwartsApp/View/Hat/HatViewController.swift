0c48ba3de903e33ace382d7c6cb9277344717c5c
Elena Diniz <elena.diniz@outlook.com>
2021-11-18 13:24:24 -0300

    Revert "Merge branch 'pr/1' into dev"
    This reverts commit 1c27e5374bb853c4f2d52b4563a7cfa31c523e26, reversing
    changes made to 2ef92a0508902b85eb3c7af1540fc21abe6ce817.

--- HogwartsApp/View/Hat/HatViewController.swift
+++ HogwartsApp/View/Hat/HatViewController.swift
@@ -7,45 +7,20 @@
 
 import UIKit
 
-protocol myHouseDelegate {
-    var myHouse: String { get }
-}
-
-class HatViewController: UIViewController, myHouseDelegate {
-    
-    var myHouse: String = ""
-    
-    
-
+class HatViewController: UIViewController {
     
     var nameHouse:[String] = ["Grifinória", "Sonserina","Corvinal", "Lufa-lufa"]
     
-    var nameHouse:[String] = ["Grifinória", "Sonserina","Corvinal", "Lufa-lufa"]
-    
-    var houseSelected: String = ""
-    
-    var house: String = ""
-    
-    @IBOutlet weak var myView: UIView!
     @IBOutlet weak var textHouse: UILabel!
     @IBOutlet weak var viewMain: GradientView!
     @IBOutlet weak var hatImageView: UIImageView!
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var suffleButton: ButtonGradient!
     @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var suffleButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.suffleButton.isHidden = false
-        self.nameTextField.isHidden = false
-        let value = UserDefaults.standard.string(forKey: "myHouse")
-        
-        
-        UserDefaults.standard.signedInUser
-        let house = UserDefaults.standard.myHouse
-        self.textHouse.text = "Sua casa sorteada foi: \(value ?? "")"
         // Do any additional setup after loading the view.
         suffleButton.layer.cornerRadius = suffleButton.frame.height / 2
         nameTextField.setEditingColor()
@@ -66,58 +41,6 @@ class HatViewController: UIViewController, myHouseDelegate {
         print(name)
         
         switch name {
-        
-        
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        
-        
-        let value = UserDefaults.standard.string(forKey: "myHouse")
-        
-        self.viewMain.firstColor = UserDefaults.UserDefaultsKeys.myHouse.backgroundColor(name: value ?? "")
-        
-    }
-    
-    @IBAction func luckButton(_ sender: UIButton) -> Void{
-    
-        
-       nameIsEmpty()
-        
-    }
-    
-    func nameIsEmpty()-> Void{
-    
-        let textName = self.nameTextField.text
-        if textName!.isEmpty{
-            self.suffleButton.isHidden = false
-            self.textHouse.text = "Preencha um nome!"
-            self.textHouse.textColor = UIColor.red
-            
-        }else{
-
-            nameHouse.shuffle()
-            
-            self.suffleButton.isHidden = true
-            self.houseSelected = nameHouse.first ?? ""
-            self.viewMain.firstColor = self.backgroundColor(name: nameHouse.first ?? "")
-            self.textHouse.text = "Sua casa sorteada foi: \(nameHouse.first ?? "")"
-            self.textHouse.textColor = UIColor.white
-            saveHouse()
-            
-            return self.nameTextField.isHidden = true
-        }
-        
-    }
-    
-    
-    func backgroundColor(name: String) -> UIColor{
-        
-        
-        myHouse = name
-        switch name {
-            
         case "Grifinória":
             return UIColor(red: 0.48, green: 0.04, blue: 0.08, alpha: 1.00)
         case "Lufa-lufa":
@@ -129,15 +52,6 @@ class HatViewController: UIViewController, myHouseDelegate {
         default:
             return .black
         }
-
-        
-    }
-    func saveHouse(){
-        // seta a string na chave selecionada
-        UserDefaults.standard.set(nameHouse.first, forKey: "myHouse")
-        // recupera a string da chave
-        let value = UserDefaults.standard.string(forKey: "myHouse")
-    
     }
     
     func saveHouse(){
@@ -148,5 +62,3 @@ class HatViewController: UIViewController, myHouseDelegate {
         print("myHouse")
     }
 }
-
-
